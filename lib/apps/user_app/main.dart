f4fb5d64a671e286e2abba79047ddbfcf4b21882
faisin111 <faisins16@gmail.com>
2026-04-07 12:29:36 +0530

    connect backend for auth section

--- lib/apps/user_app/main.dart
+++ lib/apps/user_app/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:transit_track_flutter/apps/user_app/features/bottom_bar/presentation/view/bottom_bar.dart';
-import 'package:transit_track_flutter/apps/user_app/features/landing/presentation/view/landing.dart';
-import 'package:transit_track_flutter/apps/user_app/features/save/presentation/view/save.dart';
+
 
 
 
