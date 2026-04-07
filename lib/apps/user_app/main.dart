8953e376e9f09a965082cbd00b72e641f7096e36
faisin111 <faisins16@gmail.com>
2026-04-07 12:33:19 +0530

    connect backend for auth section

--- lib/apps/user_app/main.dart
+++ lib/apps/user_app/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:transit_track_flutter/apps/user_app/features/bottom_bar/presentation/view/bottom_bar.dart';
-
-
-
-
-
 void main() {
   runApp(const UserApp());
 }
-
 class UserApp extends StatelessWidget {
   const UserApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: const BottomBar());
+    return MaterialApp();
   }
 }
