106b7d00d670ad76ce12ffd9319bd25486a910bc
Faisin S <faisins16@gmail.com>
2026-04-07 06:36:01 +0000

    change

--- lib/core/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const bg = Color.fromARGB(255, 196, 206, 210);
-  static const white = Colors.white;
-  static const black = Colors.black;
-  static const ltOrange = Color.fromARGB(255, 255, 240, 235);
-}
